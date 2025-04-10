581c4f8ad875be7b138a16ea97b4b8a0afc729ea
Edgar <edgarmasstudio@gmail.com>
2025-04-10 22:48:37 +0700

    yes

--- schema.sql
+++ schema.sql
@@ -1,1 +1,129 @@
-image.png
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for tugaslogin
+CREATE DATABASE IF NOT EXISTS `tugaslogin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `tugaslogin`;
+
+-- Dumping structure for table tugaslogin.allowed_domains
+CREATE TABLE IF NOT EXISTS `allowed_domains` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `form_id` bigint NOT NULL,
+  `domain` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `form_id` (`form_id`),
+  CONSTRAINT `allowed_domains_ibfk_1` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table tugaslogin.allowed_domains: ~0 rows (approximately)
+
+-- Dumping structure for table tugaslogin.answers
+CREATE TABLE IF NOT EXISTS `answers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `response_id` bigint NOT NULL,
+  `question_id` bigint NOT NULL,
+  `value` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `response_id` (`response_id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`response_id`) REFERENCES `responses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `answers_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table tugaslogin.answers: ~0 rows (approximately)
+
+-- Dumping structure for table tugaslogin.forms
+CREATE TABLE IF NOT EXISTS `forms` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `limit_one_response` tinyint(1) DEFAULT '0',
+  `creator_id` bigint NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `creator_id` (`creator_id`),
+  CONSTRAINT `forms_ibfk_1` FOREIGN KEY (`creator_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table tugaslogin.forms: ~0 rows (approximately)
+
+-- Dumping structure for table tugaslogin.questions
+CREATE TABLE IF NOT EXISTS `questions` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `form_id` bigint NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `choice_type` varchar(255) NOT NULL,
+  `choices` text,
+  `is_required` tinyint(1) DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `form_id` (`form_id`),
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table tugaslogin.questions: ~0 rows (approximately)
+
+-- Dumping structure for table tugaslogin.responses
+CREATE TABLE IF NOT EXISTS `responses` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `form_id` bigint NOT NULL,
+  `user_id` bigint DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `form_id` (`form_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `responses_ibfk_1` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `responses_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table tugaslogin.responses: ~0 rows (approximately)
+
+-- Dumping structure for table tugaslogin.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` datetime DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table tugaslogin.users: ~3 rows (approximately)
+INSERT IGNORE INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+	(1, 'User 1', 'user1@webtech.id', NULL, '$2a$10$79iL05c8BTBMpS96gWkyaekDM8wfL1HGK/PqvgSfRmXovXiNDbk.q', NULL, '2025-04-10 15:47:59', '2025-04-10 15:47:59'),
+	(2, 'User 2', 'user2@webtech.id', NULL, '$2a$10$G9xo4fTBFdZQkwaQ.PUsGuGAKElysjpp4HjgGOqB10h8PVk0YR2Xy', NULL, '2025-04-10 15:47:59', '2025-04-10 15:47:59'),
+	(3, 'User 3', 'user3@worldskills.org', NULL, '$2a$10$v14l99cc.kE.FCPofZqtO./F4anOpa9/1mAvzn0jCeRWeRgGMRGRa', NULL, '2025-04-10 15:47:59', '2025-04-10 15:47:59');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
